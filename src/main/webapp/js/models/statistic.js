b6c779ef7cd667800c08ecb145b55913e5de27fb
nkravche <meteokna@mail.ru>
2013-09-10 21:43:18 +0300

    update static.js

--- src/main/webapp/js/models/statistic.js
+++ src/main/webapp/js/models/statistic.js
@@ -118,10 +118,7 @@ $(function () {
         }
     });
 
-    /***********************************************
-     *    Service collection
-     * @type {*}
-     */
+
     var Service = Backbone.Model.extend(
         {
             // Default attributes for the service item
@@ -144,15 +141,14 @@ $(function () {
             parse: function (response) {
                 console.log('Parsing list of the services:' + JSON.stringify(response));
                 servicesCollection = response;
-                servicesListView.collection = servicesCollection;
-                servicesListView.render();
+                serviceListView.collection = servicesCollection;
+                serviceListView = new ServiceOptionListView().render();
                 return response;
             }
         }
     );
 
-
-    var ServicesOptionListView = Backbone.View.extend({
+    var ServiceOptionListView = Backbone.View.extend({
         template: _.template($('#services-details-template').html()),
         el: $("#services-details-block"), // DOM element with contact details,
 
@@ -162,127 +158,6 @@ $(function () {
             }))
         }
     });
-
-
-//    /**********************************************************************************
-//     **                     Views
-//     **********************************************************************************/
-//    var EmployerListView = Backbone.View.extend(
-//        {
-//
-//            template: _.template($('#employer-list-template').html()),
-//            el: $("#employer-list-block"),
-//            initialize: function (options) {
-//                _.bindAll(this, 'beforeRender', 'render', 'afterRender');
-//                var _this = this;
-//                this.render = _.wrap(this.render, function (render) {
-//                    _this.beforeRender();
-//                    render();
-//                    _this.afterRender();
-//                });
-//            },
-//
-//            beforeRender: function () {
-//                console.log('beforeRender');
-//            },
-//
-//            render: function () {
-//                $(this.el).html(this.template({employer: this.collection}));
-//                return this;
-//            },
-//
-//            afterRender: function () {
-//            }  }
-//
-//
-//    ); //Backbone.Model.extend end
-//
-//
-//    // The collection of employers
-//    var EmployerCollection = Backbone.Collection.extend(
-//        {
-//            model: Employer,
-//            url: "/beauty/employers",
-//            parse: function (response) {
-//                console.log('Parsing list of the employers:' + JSON.stringify(response));
-//                employersCollection = response;
-//                employersListView.collection = employersCollection;
-//                employersListView.render();
-//                return response;
-//            }
-//        }
-//    );
-//
-//
-//    var EmployerView = Backbone.View.extend(
-//        {
-//
-//            template: _.template($('#employer-details-template').html()),
-//            el: $("#employer-details-block"), // DOM element with contact details,
-//
-//            events: {
-//                "click  #form-btn-remove": "deleteEmployer",
-//                "click #form-btn-add": "addEmployer"
-//            },
-//            render: function () {
-//                $(this.el).html(this.template({
-//                    employer: this.model
-//                }))
-//            },
-//            deleteEmployer: function (e) {
-//                e.preventDefault();
-//                if (jQuery("#employer-details-block").valid()) {
-//                    $.ajax({
-//                        type: "post",
-//                        url: "/beauty/employers/delete", //your valid url
-//                        contentType: "application/json", //this is required for spring 3 - ajax to work (at least for me)
-//                        data: JSON.stringify(this.model), //json object or array of json objects
-//                        success: function (result) {
-//                            window.location = "/employers.html";
-//                        },
-//                        error: function () {
-//                            window.location = "/employers.html";
-//                        }
-//                    });
-//                } else {
-//                    $.fancybox.open($("#dialog"));
-//                }
-//            },
-//            addEmployer: function (e) {
-//                if (jQuery("#employer-details-block").valid()) {
-//                    var firstName = $("#inputEmployerName").val();
-//                    var surName = $('#inputEmployerSurName').val();
-//                    var lastName = $('#inputEmployerLastName').val();
-//                    var telephone = $('#inputEmployerPhone').val();
-//                    var address = $('#inputEmployerAddress').val();
-//                    var birthday = $('#inputEmployerBirthday').val();
-//                    var separationIndex = $("#selectId").val();
-//                    var separation = separationsCollection[separationIndex];
-//                    var id = $('#inputEmployerId').val();
-//
-//                    var newEmployer = new Employer(
-//                        {   "employerId": id,
-//                            "firstName": firstName,
-//                            "surName": surName,
-//                            "lastName": lastName,
-//                            "telephone": telephone,
-//                            "address": address,
-//                            "birthday": birthday,
-//                            "separation": separation}).save({}, {
-//                            wait: true,
-//                            success: function (model, response) {
-//                                window.location = "/employers.html";
-//                            },
-//                            error: function (model, error) {
-//                                window.location = "/employers.html";
-//                            }
-//                        });
-//                } else {
-//                    $.fancybox.open($("#dialog"));
-//                }
-//            }
-//        }
-//    );
 
 
     var StatisticController = Backbone.Router.extend({
@@ -310,35 +185,22 @@ $(function () {
             window.location = "/"
         }
     });
-    /**********
-     * for employers view level
-     * @type {EmployerCollection}
-     */
+
     var employersCollection = new EmployerCollection();
     var employersListView = new EmployersOptionListView({collection: employersCollection});
     employersListView.render();
 
-    /******************************
-     * Clients
-     */
+
     var clientsCollection = new ClientsCollection();
     var clientsListView = new ClientsOptionListView();
     clientsListView.render();
 
-    /**
-     * Services
-     * @type {Services}
-     */
+
     var servicesCollection = new ServiceCollection();
-    var servicesListView = ServicesOptionListView();
-    servicesListView.render();
-//
-//    var curEmployer = new Employer();
-//    var employerView = new EmployerView({model: curEmployer});
-//    employerView.render();
-//    var separationsCollection = new SeparationCollection();
-      var controller = new StatisticController();
-//    var separationListView = new SeparationView({collection: separationsCollection});
-//    separationListView.render();
+    var serviceListView = new ServiceOptionListView();
+    serviceListView.render();
+
+    var controller = new StatisticController();
+
     Backbone.history.start();
 });
